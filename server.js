a1b84a8ad51d4db34c0cda3529b6ed7e7aeb24b3
kuminson <273423480@qq.com>
2018-03-06 16:29:57 +0800

    增加http和路由

---
+++ server.js
@@ -0,0 +1,18 @@
+let http = require('http')
+let url = require('url')
+
+function start (route) {
+  function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname
+    console.log('request for ' + pathname + ' received')
+    route(pathname)
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write('hello world')
+    response.end()
+  }
+
+  http.createServer(onRequest).listen(8888)
+  console.log('server has started')
+}
+
+exports.start = start
