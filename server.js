a7f6593a8cb35e9ce2a16f8324f3a570dc209e0b
kuminson <273423480@qq.com>
2018-03-16 16:50:04 +0800

    增加http和路由

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ let url = require('url')
 
 function start (route, handle) {
   function onRequest(request, response) {
-    let postData = ''
+    // let postData = ''
     let pathname = url.parse(request.url).pathname
     console.log('request for ' + pathname + ' received')
-    request.setEncoding('utf8')
-    request.addListener('data', (postDataChunk) => {
-      postData += postDataChunk
-      console.log('received POST data chunk "' + postDataChunk + '".')
-    })
-    request.addListener('end', () => {
-      route(handle, pathname, response, postData)
-    })
+    // request.setEncoding('utf8')
+    // request.addListener('data', (postDataChunk) => {
+    //   postData += postDataChunk
+    //   console.log('received POST data chunk "' + postDataChunk + '".')
+    // })
+    // request.addListener('end', () => {
+    //   route(handle, pathname, response, postData)
+    // })
+    route(handle, pathname, response, request)
   }
 
   http.createServer(onRequest).listen(8888)
